46e2db9b5323b5af04ae4a20680f474b15fdd909
zzikzzik <a01098642164@gmail.com>
2024-05-15 17:03:48 +0900

    백준 1929(이번엔성공)

--- baekjoon1929.py
+++ baekjoon1929.py
@@ -1,21 +1,21 @@
-import math # 루트 사용
-import sys #빠른 입출력
+import math
+import sys
 print = sys.stdout.write
+input = sys.stdin.readline
 
 min,max = map(int,input().split())
-list = [2]
-i = 1
-while (2*i+1) <= max:
-    num = [2*i + 1]
-    list += num
-    i += 1
-i = 0
-while list[i] < int(math.sqrt(max)):
-    for j in list:
-        if (j%list[i] == 0) and (j != list[i]):
-            list.remove(j)
-    i += 1
-list = set(list)
-for i in range(min,max+1):
-    if i in list:
+TfList = [True for i in range(max)]
+#TfList 의 i번째 의미 : i+1은 소수인가?
+primes = list(range(2,int(math.sqrt(max))+1))
+TfList[0] = False
+for i in primes:
+    j = 2
+    while i * j <= max:
+        TfList[i*j-1] = False
+        j += 1
+
+for i in range(min,max +1):
+    if TfList[i-1]:
         print(str(i)+"\n")
+
+#5/15 : 어제 문제점 : 큰 수의 제곱근 이하의 약수로 나는게 아닌 리스트 속 요소들로만 나누었음.
